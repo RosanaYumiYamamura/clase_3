4bff3b0702b30c724e5b02863fe3672f2eaff251
Rosana Yumi Yamamura <127357201+RosanaYumiYamamura@users.noreply.github.com>
2023-03-27 20:13:25 -0300

    Delete par_impar_v1.ts

--- par_impar_v1.ts
+++
@@ -1,9 +0,0 @@
-import *as rls from "readline-sync"
-let nro : number =  rls.questionInt ("Ingrese un numero:");
-if (nro == 0) {console.log("el numero es cero");
-} else if (nro % 2 == 0) {
-    console.log("El numero", nro , "es par");
-    }
-    else {
-    console.log("el numero",nro, "es impar");
-    } 
