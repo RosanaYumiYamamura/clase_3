f72e027526c99771e48742714a3b79a4a33e2a0a
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-26 23:43:17 -0300

    first commit

---
+++ PyDenVariable.ts
@@ -0,0 +1,7 @@
+import * as rls from "readline-sync";
+let primerNumero : number = 450.50;
+console.log("el primer número es ", primerNumero);
+let segundoNumero : number = 10;
+console.log("el segundo número es ", segundoNumero);
+let resultado : number = primerNumero * segundoNumero/100;
+console.log("El resultado de la suma es ", resultado);
