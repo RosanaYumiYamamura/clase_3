d21f4ae24dae9a5ea49fe3a7c6300f947cee7852
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-27 00:34:11 -0300

    new

---
+++ xMedalla.ts
@@ -0,0 +1,14 @@
+let posicionLlegada : number = readlineSync.questionInt ("Ingrese la posición de llegada del competidor: ");
+switch (posicionLlegada) {
+case 1:
+console.log("Entregar medalla de oro");
+break;
+case 2:
+console.log("Entregar medalla de plata");
+break;
+case 3:
+console.log("Entregar medalla de bronce");
+break;
+default:
+console.log("Entregar mención de participación");
+}
